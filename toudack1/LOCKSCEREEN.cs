3559967a5fd9ccd07429bd360f5cd1c06aa09374
hamedb2020 <hamedb2020@gmail.com>
2019-10-25 03:27:52 +0330

    add lockscreen

---
+++ toudack1/LOCKSCEREEN.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace toudack1
+{
+    public partial class LOCKSCEREEN : Form
+    {
+        public LOCKSCEREEN()
+        {
+            InitializeComponent();
+        }
+
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int
+           wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowsHookExA", CharSet = CharSet.Ansi)]
+        private static extern int SetWindowsHookEx(
+           int idHook,
+           LowLevelKeyboardProcDelegate lpfn,
+           int hMod,
+           int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern int UnhookWindowsHookEx(int hHook);
+
+        [DllImport("user32.dll", EntryPoint = "CallNextHookEx", CharSet = CharSet.Ansi)]
+        private static extern int CallNextHookEx(
+            int hHook, int nCode,
+            int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        const int WH_KEYBOARD_LL = 13;
+        private int intLLKey;
+        private KBDLLHOOKSTRUCT lParam;
+
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+
+        private int LowLevelKeyboardProc(
+            int nCode, int wParam,
+            ref KBDLLHOOKSTRUCT lParam)
+        {
+            bool blnEat = false;
+            switch (wParam)
+            {
+                case 256:
+                case 257:
+                case 260:
+                case 261:
+                    //Alt+Tab, Alt+Esc, Ctrl+Esc, Windows Key
+                    if (((lParam.vkCode == 9) && (lParam.flags == 32)) ||
+                    ((lParam.vkCode == 27) && (lParam.flags == 32)) || ((lParam.vkCode ==
+                    27) && (lParam.flags == 0)) || ((lParam.vkCode == 91) && (lParam.flags
+                    == 1)) || ((lParam.vkCode == 92) && (lParam.flags == 1)) || ((true) &&
+                    (lParam.flags == 32)))
+                    {
+                        blnEat = true;
+                    }
+                    break;
+            }
+
+            if (blnEat)
+                return 1;
+            else return CallNextHookEx(0, nCode, wParam, ref lParam);
+
+        }
+
+        private void KeyboardHook(object sender, EventArgs e)
+        {
+            intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL, new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc),
+                       System.Runtime.InteropServices.Marshal.GetHINSTANCE(
+                       System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0]).ToInt32(), 0);
+        }
+
+        private void ReleaseKeyboardHook()
+        {
+            intLLKey = UnhookWindowsHookEx(intLLKey);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+           // WindowState = FormWindowState.Maximized;
+            this.WindowState = FormWindowState.Maximized;
+            KeyboardHook(this, e);
+        }
+
+        private void webBrowser1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            KeyboardHook(this, e);
+        }
+    }
+}
--- toudack1/Properties/Resources.Designer.cs
+++ toudack1/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace toudack1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_lock_512 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-lock-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
